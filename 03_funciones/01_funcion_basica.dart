0bbaede56a66f34d87c8321470bd51b9f64d4016
Rodrigo <rorro.poblete@gmail.com>
2023-09-13 16:44:09 -0300

    feat: add functions basic section

---
+++ 03_funciones/01_funcion_basica.dart
@@ -0,0 +1,8 @@
+void main() {
+  String saludo = "hola";
+
+  String nombre = "Qevin";
+
+  print(saludo);
+  print(nombre);
+}
---
+++ 03_funciones/02_argumentos.dart
@@ -0,0 +1,14 @@
+void saludar(String mensaje,
+    [String? nombre = '<insertar nombre>', int edad = 20]) {
+  print('$mensaje $nombre - $edad');
+}
+
+void saludar2(String mensaje, {required String nombre, int veces = 10}) {
+  print('Saludar2: $mensaje $nombre - $veces');
+}
+
+void main(List<String> args) {
+  saludar('Hola', 'Rod', 30);
+
+  saludar2('Saludos', veces: 20, nombre: 'Tony');
+}
