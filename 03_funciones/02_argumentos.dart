53875b073526df976a901bccfa5d523adc86e42c
Rodrigo Poblete <rorro.poblete@gmail.com>
2023-09-24 23:31:22 -0300

    feat: add desciption title functions

--- 03_funciones/02_argumentos.dart
+++ 03_funciones/02_argumentos.dart
@@ -3,9 +3,16 @@ void saludar(String mensaje,
   print('$mensaje $nombre - $edad');
 }
 
+// Argumentos con nombre (Named parameters) required y opcional
 void saludar2(String mensaje, {required String nombre, int veces = 10}) {
   print('Saludar2: $mensaje $nombre - $veces');
 }
+
+// Funciones de flecha
+String greetEveryone() => 'Hello everyone';
+
+// Función de flecha con parámetros
+int addTwoNumbers(int a, int b) => a + b;
 
 void main(List<String> args) {
   saludar('Hola', 'Rod', 30);
